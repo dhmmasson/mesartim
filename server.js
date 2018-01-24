aa757a205615978f59a988cac67410c59036c109
dhmmasson <dhm.masson+github@gmail.com>
2018-01-24 18:44:15 +0100

    moved landing page after registration

--- server.js
+++ server.js
@@ -828,7 +828,7 @@ function processNewParticapation( requete, reponse, result ){
 								, { expiresIn: "8d" // expires in 24 hours    
 								} ) ;
 	reponse.cookie( 'token', token );
-	reponse.redirect('/generation')
+	reponse.redirect('/vote')
 
 }
 //List séances 
