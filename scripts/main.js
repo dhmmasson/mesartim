a0806c2d0eaa1f84edf82b77799c71cf80a0a098
dhmmasson <dhm.masson+github@gmail.com>
2020-10-23 13:03:44 +0200

    Update ref to Jquery-ui

--- scripts/main.js
+++ scripts/main.js
@@ -3,22 +3,22 @@ requirejs.config({
     requirejs: '../bower_components/requirejs/require',
     socketio: '/socket.io/socket.io.js',
     jquery: '../bower_components/jquery/dist/jquery',
-    'jqueryui': '../bower_components/jquery-ui',
+    'jqueryui': '../node_modules/jquery-ui',
     'materialize': '../../bower_components/Materialize/dist/js/materialize',
     'hammerjs':    '../../bower_components/Materialize/js/hammer.min',
     'jquery-hammerjs':'../../bower_components/Materialize/js/jquery.hammer',
     'date_format' :  '../../bower_components/date-format/dist/date-format.min',
-    'd3' :  '../../bower_components/d3/d3.min',        
+    'd3' :  '../../bower_components/d3/d3.min',
 		'papaparse' : '../../bower_components/papaparse/papaparse.min'
   },
   packages: [
 
   ],
   shim: {
-		
+
     socketio: {
       exports: 'io'
-    }, 
+    },
     'materialize': {
       deps: ['jquery', 'hammerjs', 'jquery-hammerjs']
     },
@@ -29,11 +29,11 @@ requirejs.config({
       //exports: "$",
       deps: ['jquery']
     }
-    , 
+    ,
     'date_format': {
       exports : "date_format"
     }
-		, 'papaparse' : { exports : "Papa" } 
+		, 'papaparse' : { exports : "Papa" }
   }
 });
 
@@ -41,10 +41,10 @@ requirejs.config({
 requirejs(["date_format", 'jquery', 'socketio', 'materialize', 'util', 'jqueryui/ui/sortable', 'visualisation', 'screen', 'csv' ], mainFunction )
 
 function mainFunction (io, mdl, util, ui) {
-	token = localStorage.getItem('token') || null ; 
-	since = 0 
+	token = localStorage.getItem('token') || null ;
+	since = 0
 
-	//Activate the tab according to url 
+	//Activate the tab according to url
   $(function() {
     var pgurl = window.location.href.substr(window.location.href.lastIndexOf("/")+1);
     $("nav ul li a").each(function(){
@@ -53,23 +53,23 @@ function mainFunction (io, mdl, util, ui) {
     })
 			});
 
-	
+
   page = location.pathname.slice(1);
-  //ALL 
+  //ALL
 	$(".button-collapse").sideNav();
   if( page == "admin" ) {
 		visualisation.loadData() ;
   }
 	if( page == "screen" ) {
 		screen.init() ;
-	}  
+	}
   if( page == "login" || page == "") {
     //registration
     $.get("seance", processListSeances )
     $('select').material_select();
   } else {
 
-    //pour generation et vote 
+    //pour generation et vote
     getSeanceColumnAndRows( init ) ;
   }
 
@@ -84,15 +84,15 @@ function mainFunction (io, mdl, util, ui) {
 			//vote
 			getAllIdeas(0) ;
 			$("form.vote").submit( voteIdea ) ;
-			$("input.mesarthimSlider").mesartimSlider() 
-			restoreVote() 
+			$("input.mesarthimSlider").mesartimSlider()
+			restoreVote()
 		} else if ( page == "rank") {
 			addVoteValue()
-			$("input.mesarthimSlider").mesartimSlider() 
+			$("input.mesarthimSlider").mesartimSlider()
 			$(".ideas").mesartimSortable(".criteriaSorter")
-			
+
 		} else if ( page == "screenResult") {
-			console.log( "ScreenResult" ) 
+			console.log( "ScreenResult" )
 			getScreenInfo();
 		}
 
@@ -108,11 +108,11 @@ function mainFunction (io, mdl, util, ui) {
 	}
 	function processVoteValue( data ) {
 		for (var i = 0; i < data.result.length; i++) {
-			voteData = data.result[i] ;    
+			voteData = data.result[i] ;
 			voteElement = $("#idea_" + voteData.message_id ) ;
-			voteElement.data( "criteria_"+voteData.criteria_id , voteData.value ) ;    
+			voteElement.data( "criteria_"+voteData.criteria_id , voteData.value ) ;
 		}
-		$($(".criteriaSorter")[2]).click() 
+		$($(".criteriaSorter")[2]).click()
 	}
 
 
@@ -122,13 +122,13 @@ function mainFunction (io, mdl, util, ui) {
 	//===============================================================
 	function voteIdea( e ){
 
-		
+
 
 		e.preventDefault();
 		var $form = $( this )
 		, data =  $form.serializeArray()
 		, $header= $form.parent("li.idea")
-		, message_id = $form.data("message_id")  
+		, message_id = $form.data("message_id")
 
 		$header.addClass("voted")
 
@@ -141,17 +141,17 @@ function mainFunction (io, mdl, util, ui) {
 			var tab = data[i].name.match(/criteria_(\d+)/) || []
 			, ok = tab[1]
 			, id = tab[1] ;
-			
+
 			if( ok ){
 				criteria.push(
 					{ message_id : message_id
-						, criteria_id : id 
+						, criteria_id : id
 						, value : data[i].value
 					})
 				if( id == 0 ) $header.addClass("interested")
 			}
 		}
-		
+
 
 		$.post("/api/message/"+ message_id +"/annotate", { criteria : criteria }, processVoteIdea ) ;
 		$(this).parent( ".collapsible-header").trigger("click.collapse") ;
@@ -164,16 +164,16 @@ function mainFunction (io, mdl, util, ui) {
 	function processRestoreVote( data ) {
 		console.log( data )
 		for( var i = 0 ; i < data.result.length ; i++ ) {
-			var vote = data.result[i] 
+			var vote = data.result[i]
 			, $slider = $("#idea_" + vote.message_id + "_criteria_" + vote.criteria_id )
 			, $header= $slider.parents("li.idea").addClass("voted")
-			
-			if( vote.criteria_id == 0 ) $header.addClass("interested")    
+
+			if( vote.criteria_id == 0 ) $header.addClass("interested")
 			$slider.val( vote.value )
-			$slider.change() 
+			$slider.change()
 		}
 
-		
+
 	}
 	function processVoteIdea( res ){
 
@@ -182,12 +182,12 @@ function mainFunction (io, mdl, util, ui) {
 	//===============================================================
 	//SOUMETTRE UNE IDÉE AU SERVEUR=====================
 	//===============================================================
-	function addIdea(e) {       
+	function addIdea(e) {
 		var message =  $( this ).serialize()
-		
+
 		if (message.content != "" )
 			$.post("api/message/new", $( this ).serialize() , processAddIdea )
-		
+
 		e.preventDefault();
 	}
 	function processAddIdea( res ) {
@@ -195,7 +195,7 @@ function mainFunction (io, mdl, util, ui) {
 			$("#ideaSubmissionField").val("")
 			getOwnIdea( since ) ;
 		}
-	}   
+	}
 
 
 	//===============================================================
@@ -203,8 +203,8 @@ function mainFunction (io, mdl, util, ui) {
 	//===============================================================
 	function categoryChosen( e ) {
 		var radio = $(e.target)
-		, columnIndex =  radio.data( "columnindex" ) 
-		, rowIndex    =  radio.data( "rowindex" )       
+		, columnIndex =  radio.data( "columnindex" )
+		, rowIndex    =  radio.data( "rowindex" )
 		, columnName  =  radio.data( "columnname" )
 		, rowName     =  radio.data( "rowname" )
 
@@ -218,7 +218,7 @@ function mainFunction (io, mdl, util, ui) {
 
 
 	function changeTitle( rowName, columnName ){
-		var titre = "Catégorie sélectionnée : "+ rowName + "/" + columnName 
+		var titre = "Catégorie sélectionnée : "+ rowName + "/" + columnName
 		$("#sectionSelectionTitle").text( titre )
 		titre = "Ajoutez une idée à la catégorie : " + rowName+ "/" + columnName
 		$("#addIdeaTitle")
@@ -227,12 +227,12 @@ function mainFunction (io, mdl, util, ui) {
 	}
 
 	function highlightHeadersAndCell(radio, rowIndex, columnIndex ) {
-		var table = $(radio.parents("table")[0] ) 
-		, baseName = table.data("basename")      
+		var table = $(radio.parents("table")[0] )
+		, baseName = table.data("basename")
 		//unhiglight all cells and headers
 		table.find("th,td").removeClass("active")
 		//Highlight the current cell
-		$(radio.parents("td")[0] ).addClass("active"); 
+		$(radio.parents("td")[0] ).addClass("active");
 		//Highlight header
 		$("#" + baseName + "_column_" + columnIndex).addClass("active")
 		$("#" + baseName + "_row_" +   rowIndex ).addClass("active")
@@ -241,14 +241,14 @@ function mainFunction (io, mdl, util, ui) {
 	function hideIdeasDifferentCategory( rowIndex, columnIndex ) {
 		$(".idea:not(.row_"+rowIndex+".col_"+columnIndex+")").filter(":not(.hidden)")
 			.addClass("hidden")
-			.slideUp( 500 )                      
+			.slideUp( 500 )
 		$(".idea.hidden.row_"+rowIndex+".col_"+columnIndex)
-			.removeClass("hidden") 
-			.slideDown(400)        
+			.removeClass("hidden")
+			.slideDown(400)
 	}
 	function propagateCellClickToInput() {
 		inputElement = $( $(this).find("input")[0] )
-		inputElement.prop("checked", true ) 
+		inputElement.prop("checked", true )
 		inputElement.change() ;
 	}
 	function setIdeaSubmissionOk() {
@@ -270,9 +270,9 @@ function mainFunction (io, mdl, util, ui) {
 	function processListSeances( res ) {
 		seance =  $("#seance")
 		for( var i = 0 ; i < res.length ; i++ ){
-			seance.append( "<option value='"+res[i].id+"'>"+res[i].titre+"</option>" )            
+			seance.append( "<option value='"+res[i].id+"'>"+res[i].titre+"</option>" )
 		}
-		seance.material_select()          
+		seance.material_select()
 	}
 
 
@@ -284,13 +284,13 @@ function mainFunction (io, mdl, util, ui) {
 
 	function getOwnIdea( since ) {
 		$.get( "api/message/mine"
-					 , { 
+					 , {
 						 since : since }
 					 , processGetOwnIdea ) ;
 	}
 
 	function getScreenInfo( ) {
-		$.get( "api/screen/info", processScreen ) 
+		$.get( "api/screen/info", processScreen )
 	}
 	function processScreen( res ) {
 		letters = "ABCDFGHIJKLMNOPQRSTUVWXYZ"
@@ -303,7 +303,7 @@ function mainFunction (io, mdl, util, ui) {
 				if( !messages[ item.message_id ] ) {
 					messages[ item.message_id ] = { row : item.row_position, column : item.column_position, screen : {} }
 				}
-				message = 	messages[ item.message_id ] 
+				message = 	messages[ item.message_id ]
 				if( message.screen[ item.description ] === undefined ) message.screen[ item.description ] =0
 				message.screen[ item.description ] +=  item.count
 			}
@@ -318,13 +318,13 @@ function mainFunction (io, mdl, util, ui) {
 						max = message.screen[j]
 					}
 				}
-				var pos =  message.row +"_"+message.column 
+				var pos =  message.row +"_"+message.column
 				grilleElement = $("#grille_label_"+pos )
 
 				grilleElement.data( desc , ( grilleElement.data( desc ) || 0 ) + 1 )
 				var text = ""
 				for( var t of ["vote", "discuss"] ) {
-					
+
 					text += t + ": " +   (grilleElement.data( t )||0) + "<br>"
 					if( message.screen[t] > 0 ) {
 						$idea = $("#idea_"+i + " ." + t)
@@ -334,40 +334,40 @@ function mainFunction (io, mdl, util, ui) {
 				}
 				grilleElement.html( text )
 
-				
+
 			}
 		}
 	}
 
-	function processGetOwnIdea( res ) { 
-		letters = "ABCDFGHIJKLMNOPQRSTUVWXYZ"  
+	function processGetOwnIdea( res ) {
+		letters = "ABCDFGHIJKLMNOPQRSTUVWXYZ"
 		if( res.success ) {
 			console.log( res )
 			ownIdea = $("#ownIdea")
 			for( var i = 0 ; i < res.result.length ; i++ ) {
 				var item = res.result[i]
-				, newBadge = $("<span class='mesartimNew'>").text("new") 
-				ownIdea.prepend( 
+				, newBadge = $("<span class='mesartimNew'>").text("new")
+				ownIdea.prepend(
 					$("<li>" )
-						.addClass("collection-item row mesartimIdeaList valign-wrapper") 
+						.addClass("collection-item row mesartimIdeaList valign-wrapper")
 						.append( $("<i>")
 										 .addClass("left")
-										 .text(item.id) )      
+										 .text(item.id) )
 						.append( $("<span>")
 										 .addClass( "ideaText valign")
 										 .text( item.text ) )
 						.append( newBadge )
 						.append( $("<a>")
-										 .addClass("mesartimChip clickable waves-effect waves-light")            
-										 .text( letters[item.row_position] + ( item.column_position + 1 ))  
+										 .addClass("mesartimChip clickable waves-effect waves-light")
+										 .text( letters[item.row_position] + ( item.column_position + 1 ))
 										 .attr( "title", rowNames[item.row_position].title  + "/" + columnNames[item.column_position].title )
-										 .data( "category", $("#grille_radio_"+item.row_position+"_"+ item.column_position )) 
-										 .click( function(e) { 
+										 .data( "category", $("#grille_radio_"+item.row_position+"_"+ item.column_position ))
+										 .click( function(e) {
 
 											 $(e.target).data( "category").prop("checked", true).change() })
 									 )
 
-          
+
 
 				) ;
 				newBadge.fadeOut(5000, function() { $(this).remove(); })
@@ -388,8 +388,8 @@ function mainFunction (io, mdl, util, ui) {
   }
   function processSeanceColumnAndRows( data ) {
     if( data.success ) {
-      rowNames = data.rowNames ; 
-      columnNames = data.columnNames ; 
+      rowNames = data.rowNames ;
+      columnNames = data.columnNames ;
     }
   }
 
